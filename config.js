c98d0e3811e9548c3e9418354a7872d47f95e9ba
Ryandm789 <71458861+Ryandm789@users.noreply.github.com>
2021-01-08 01:46:23 -0600

    update Mapping

---
+++ config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = pk.eyJ1Ijoicnlhbjc4OSIsImEiOiJja2pubWdsZW0wZXFhMnZwcnJ4a214b2MzIn0.glVzl6d7dgP3Vc_3TW_Deg
---
+++ logic.js
@@ -0,0 +1,9 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
